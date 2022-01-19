6a253d1385d27b985a5cedc830a59674da920a36
Skopkin <skopkin@aol.com>
2022-01-19 12:35:58 -0500

    Javadoc Update
    -Added Javadoc to Assignment 2
    -Also fixed a typo in the output

--- src/assignment2/fibSequence.java
+++ src/assignment2/fibSequence.java
@@ -1,14 +1,33 @@
 package assignment2;
 
+/**
+ * Class that defines a method to calculate the nth term in the Fibonacci Sequence.
+ * 
+ * @author Sam Kopkin
+ *
+ */
 public class fibSequence {
+	/** 
+	 * Main method of the program. The Nth term to be calculated is hard-coded for now.
+	 * This method will call the recursiveFibonacci method using the nth value given
+	 * and print out the result to the console.
+	 * 
+	 * @param args Default String[] argument for main method
+	 */
 	public static void main(String[] args) {
 		final int nth = 10;
 		
 		int result = recursiveFibonacci(nth - 1);
 		
-		System.out.println("The " + nth + "th term of the Fibonnacci sequencec is: " + result);
+		System.out.println("The " + nth + "th term of the Fibonacci sequencec is: " + result);
 	}
 	
+	/**
+	 * A recursive method that returns the nth term of the Fibonacci sequence.
+	 * 
+	 * @param n The nth value of the Fibonacci sequence to calculate up to
+	 * @return Returns the nth term of the Fibonacci sequence
+	 */
 	private static int recursiveFibonacci(int n) {
 		
 		if (n <= 1) { 
